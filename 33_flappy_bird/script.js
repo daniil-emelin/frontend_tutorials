5f2970019768e52e5e930a7a9aaaa1c21e96b8a7
Daniil Emelin <daniilyemelin@gmail.com>
2024-02-02 10:43:05 +0400

    fix flappy bird js

--- 33_flappy_bird/script.js
+++ 33_flappy_bird/script.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-undef */
 kaboom();
 
-const BACKGROUND_COLOR = "#b6e5ea";
-const PIPE_COLOR = "#74c02e";
+const GRAVITY = 3200;
+const WIDTH = width();
+const HEIGHT = height();
+
+const BACKGROUND_COLOR = Color.fromHex("#b6e5ea");
+const PIPE_COLOR = Color.fromHex("#74c02e");
 
 const PIPE_WIDTH = 64;
 const PIPE_BORDER = 4;
@@ -16,50 +20,50 @@ const CEILING = -60;
 let score = 0;
 
 loadSprite("bird", "/sprites/bird.png");
+
 loadSound("score", "/sounds/score.mp3");
-loadSound("wooosh", "/sounds/wooosh.mp3");
+loadSound("jump", "/sounds/jump.mp3");
 loadSound("hit", "/sounds/hit.mp3");
 
-setGravity(3200);
-setBackground(Color.fromHex(BACKGROUND_COLOR));
+setGravity(GRAVITY);
+setBackground(BACKGROUND_COLOR);
 
 scene("game", () => {
   const game = add([timer()]);
 
-  const createPlayer = () => {
-    const player = game.add([
+  const createBird = () => {
+    const bird = game.add([
       sprite("bird"),
-      pos(width() / 4, 0),
+      pos(WIDTH / 4, 0),
       area(),
       body(),
     ]);
 
-    return player;
+    return bird;
   };
 
-  const bird = createPlayer();
+  const bird = createBird();
 
-  onKeyPress("space", () => {
+  const jump = () => {
     bird.jump(JUMP_FORCE);
-    play("wooosh");
-  });
+    play("jump");
+  }
 
-  onClick(() => {
-    bird.jump(JUMP_FORCE);
-    play("wooosh");
-  });
+  onKeyPress("space", jump);
+  onClick(jump);
 
-  const spawnPipe = () => {
+  const createPipes = () => {
     const bottomPipeHeight = rand(
       PIPE_MIN_HEIGHT,
-      height() - PIPE_MIN_HEIGHT - PIPE_OPEN
+      HEIGHT - PIPE_MIN_HEIGHT - PIPE_OPEN
     );
-    const topPipeHeight = height() - bottomPipeHeight - PIPE_OPEN;
+
+    const topPipeHeight = HEIGHT - bottomPipeHeight - PIPE_OPEN;
 
     game.add([
       pos(width(), 0),
       rect(PIPE_WIDTH, bottomPipeHeight),
-      color(Color.fromHex(PIPE_COLOR)),
+      color(PIPE_COLOR),
       outline(PIPE_BORDER),
       area(),
       move(LEFT, SPEED),
@@ -68,9 +72,9 @@ scene("game", () => {
     ]);
 
     game.add([
-      pos(width(), bottomPipeHeight + PIPE_OPEN),
+      pos(WIDTH, bottomPipeHeight + PIPE_OPEN),
       rect(PIPE_WIDTH, topPipeHeight),
-      color(Color.fromHex(PIPE_COLOR)),
+      color(PIPE_COLOR),
       outline(PIPE_BORDER),
       area(),
       move(LEFT, SPEED),
@@ -80,10 +84,12 @@ scene("game", () => {
     ]);
   };
 
-  game.loop(1, spawnPipe);
+  game.loop(1, createPipes);
 
   bird.onUpdate(() => {
-    if (bird.pos.y >= height() || bird.pos.y <= CEILING) {
+    const birdPosY = bird.pos.y;
+
+    if (birdPosY >= HEIGHT || birdPosY <= CEILING) {
       go("lose", score);
     }
   });
@@ -96,6 +102,7 @@ scene("game", () => {
   onUpdate("pipe", (pipe) => {
     if (pipe.pos.x + pipe.width <= bird.pos.x && pipe.passed === false) {
       addScore();
+
       pipe.passed = true;
     }
   });
@@ -104,7 +111,7 @@ scene("game", () => {
     const scoreLabel = game.add([
       text(score),
       anchor("center"),
-      pos(width() / 2, 80),
+      pos(WIDTH / 2, 80),
       fixed(),
       z(100),
     ]);
@@ -117,6 +124,7 @@ scene("game", () => {
   const addScore = () => {
     score++;
     scoreLabel.text = score;
+
     play("score");
   };
 });
